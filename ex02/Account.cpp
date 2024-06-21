3c17e19f7df992a8a309acdd608ada6837f3aa9e
CarolinaRVB <carolinavbastos@hotmail.com>
2024-06-21 13:00:33 +0100

    starting last exercise

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,7 +6,26 @@
 /*   By: crebelo- <crebelo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 11:07:25 by crebelo-          #+#    #+#             */
-/*   Updated: 2024/06/17 11:07:26 by crebelo-         ###   ########.fr       */
+/*   Updated: 2024/06/19 17:01:45 by crebelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+
+int  Account::getNbAccounts( void ){
+	return (_nbAccounts);
+}
+
+int Account::getTotalAmount( void ){
+	return (_totalAmount);
+}
+
+int Account::getNbDeposits( void ){
+	return (_totalNbDeposits);
+}
+
+int Account::getNbWithdrawals( void ){
+	return (_totalNbWithdrawals);
+}
 
